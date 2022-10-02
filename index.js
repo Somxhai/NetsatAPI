25271d65ef0b474241560ccc498477576d52c96f
Somxhai <ChanakanThongrong@gmail.com>
2022-10-02 21:37:26 +0700

    move details to NetsatAPI folder

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ app.listen(4000);
 const method = require("./method");
 
 const netsatPath = require("./NetsatAPI/netsat.json");
+const details = require("./NetsatAPI/details.json")
 
-const mergedData = {
-  syllabusCount: 169,
-  facultyCount: 23,
-  dataDate: "16-11-2564",
-};
 app.get("/", (req, res) => {
   const data = {
-    ...mergedData,
+    ...details,
     netsat_data: netsatPath,
   };
   res.status(200).json(data);
 });
 
 app.get("/details", (req, res) => {
-  res.status(200).json(mergedData);
+  res.status(200).json(details);
 });
 
 app.get("/netsat", (req, res) => {
