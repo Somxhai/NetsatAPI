70faf55b34696ddd805f74c2df2e84467d823ddf
Somxhai <ChanakanThongrong@gmail.com>
2022-10-02 21:03:11 +0700

    add new routes and remove unused packages

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const app = require("express")();
 const cors = require("cors");
 app.use(cors());
-const PORT = 4000
+const PORT = 4000;
 
-app.listen(PORT);
+app.listen(PORT, ()=>console.log(`API LISTENING ON PORT ${PORT}`));
 
 const method = require("./method");
 
 const netsatPath = require("./NetsatAPI/netsat.json");
 
-const syllabusCount = 169;
-const facultyCount = 23;
-const dataDate = "16-11-2564";
 const mergedData = {
-  syllabusCount: syllabusCount,
-  facultyCount: facultyCount,
-  dataDate: dataDate,
+  syllabusCount: 169,
+  facultyCount: 23,
+  dataDate: "16-11-2564",
 };
-app.get("/netsat", (req, res) => {
+app.get("/", (req, res) => {
   const data = {
     ...mergedData,
     netsat_data: netsatPath,
@@ -25,9 +22,19 @@ app.get("/netsat", (req, res) => {
   res.status(200).json(data);
 });
 
-app.get("/netsat/id/:id", (req, res) => {
+app.get("/details", (req, res) => {
+
+  res.status(200).json(mergedData);
+});
+
+app.get("/netsat", (req, res) => {
+
+  res.status(200).json(netsatPath);
+});
+
+app.get("/:id", (req, res) => {
   const id = req.params.id;
   res.status(200).json(method.getSyllabusById(id, netsatPath));
 });
 
-module.exports = app
+module.exports = app;
